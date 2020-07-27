feaf38512e05fa0a0fe3b9bb7248fd3338ddb992
Shango9947 <45555868+Shango9947@users.noreply.github.com>
2020-07-28 02:40:48 +0530

    Update Suffix_Array_LCP.h

--- IMP_APP/Suffix_Array_LCP.h
+++ IMP_APP/Suffix_Array_LCP.h
@@ -51,24 +51,24 @@ vector<int> suffix_array_construction(string s) {
     sorted_shifts.erase(sorted_shifts.begin());
     return sorted_shifts;
 }
-vi rank;
+vi rlulu;
 vector<int> kasai (string const& s, vector<int> const& p) {
     int n = s.size();
-    rank.resize(n, 0);
+    rlulu.resize(n, 0);
     for (int i = 0; i < n; i++)
-        rank[p[i]] = i;
+        rlulu[p[i]] = i;
 
     int k = 0;
     vector<int> lcp(n-1, 0);
     for (int i = 0; i < n; i++) {
-        if (rank[i] == n - 1) {
+        if (rlulu[i] == n - 1) {
             k = 0;
             continue;
         }
-        int j = p[rank[i] + 1];
+        int j = p[rlulu[i] + 1];
         while (i + k < n && j + k < n && s[i+k] == s[j+k])
             k++;
-        lcp[rank[i]] = k;
+        lcp[rlulu[i]] = k;
         if (k)
             k--;
     }
